5710720b2f1bf4afaa8f3cfe269f7d33e1983b0b
Hüseyin Aslan <btdhaslan@gmail.com>
2025-05-12 16:20:17 +0300

    proje tekrardan local'e alındı

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,14 +4,11 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   surname: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true, select: false },
+  password: { type: String, required: true },
   birthday: { type: Date },
-  gender: { type: String, enum: ['male', 'female', 'other'] },
+  gender: { type: String },
   isVerified: { type: Boolean, default: false },
   verificationToken: { type: String },
-  wishlist: [String],
-likes: [String],
-favorites: [String],
 }, { timestamps: true });
 
 module.exports = mongoose.model("User", userSchema);
